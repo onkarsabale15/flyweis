05bc1cdbdb1fb8a28aa3c35472fdee9898632fce
HerbsMagic <136325970+HerbsMagic@users.noreply.github.com>
2024-03-18 15:56:15 +0530

    d

--- route/routes/auth.js
+++ route/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../../controller/index");
 
-router.get("/api/otp/:mobileNumber", controllers.authController.sendOtp);
+router.get("/api/otp/send/:mobileNumber", controllers.authController.sendOtp);
 router.post("/api/otp/verify", controllers.authController.verifyOtp);
 router.get("/api/otp/resend/:mobileNumber", controllers.authController.sendOtp)
 module.exports = router;
